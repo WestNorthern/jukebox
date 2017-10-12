41ef8cea72043b354f3066ed1d3655ba34e71c5e
westnorthern <rrichardson610@gmail.com>
2017-10-12 14:29:07 -0600

    moved all of the buttons inside the constructor function so that they are created with jukebox instance

--- jukebox.js
+++ jukebox.js
@@ -3,9 +3,57 @@ class Jukebox {
     this.playlist = playlist;
     this.songNo = 0;
     this.current = this.playlist[this.songNo];
+
+    // Create buttons list
+
+    // Play Button
+    let play = document.createElement('div');
+		play.textContent = "Play";
+		play.setAttribute('id', 'play');
+		document.body.appendChild(play);
+		play.addEventListener('click', () => { this.playSong() });
+		// End Play Button
+
+		// Pause Button
+    let pause = document.createElement('div');
+		pause.textContent = "Pause";
+		pause.setAttribute('id', 'play');
+		document.body.appendChild(pause);
+		pause.addEventListener('click', () => { this.pauseSong() });
+		// End Play Button
+
+
+		// Stop Button
+    let stop = document.createElement('div');
+		stop.textContent = "Stop";
+		stop.setAttribute('id', 'stop');
+		document.body.appendChild(stop);
+		stop.addEventListener('click', () => { this.loadSong(); });
+		// End Play Button
+
+
+
+		// Next Button
+    let next = document.createElement('div');
+		next.textContent = "Next";
+		next.setAttribute('id', 'next');
+		document.body.appendChild(next);
+		next.addEventListener('click', () => { this.nextSong() });
+		// End Play Button
+
+
+		// Back Button
+    let back = document.createElement('div');
+		back.textContent = "Back";
+		back.setAttribute('id', 'back');
+		document.body.appendChild(back);
+		back.addEventListener('click', () => { this.previousSong() });
+		// End Play Button
+
     
     this.audioElement = document.createElement('audio');
     this.loadSong();
+
 
     // Goes to next song (requires ES 6 arrow function)
 
@@ -101,36 +149,36 @@ let songArray = ['songs/Anitek.mp3', 'songs/CigBrek.mp3',
 
 let myJuke = new Jukebox(songArray);
 
-let play = document.getElementById('play');
-let pause = document.getElementById('pause');
-let stop = document.getElementById('stop');
-let next = document.getElementById('next');
-let last = document.getElementById('last');
+// // let play = document.getElementById('play');
+// // let pause = document.getElementById('pause');
+// let stop = document.getElementById('stop');
+// let next = document.getElementById('next');
+// let last = document.getElementById('last');
 
-stop.addEventListener('click', function(){
+// stop.addEventListener('click', function(){
 
-	myJuke.loadSong();
-	document.body.style.background = 'red';
+// 	myJuke.loadSong();
+// 	document.body.style.background = 'red';
 
-});
+// });
 
-play.addEventListener('click', function(){
+// play.addEventListener('click', function(){
 
-	myJuke.playSong();
+// 	myJuke.playSong();
 
-});
+// });
 
-pause.addEventListener('click', function(){
+// pause.addEventListener('click', function(){
 
-	myJuke.pauseSong();
-});
+// 	myJuke.pauseSong();
+// });
 
-next.addEventListener('click', function(){
+// next.addEventListener('click', function(){
 
-	myJuke.nextSong();
-});
+// 	myJuke.nextSong();
+// });
 
-last.addEventListener('click', function(){
+// last.addEventListener('click', function(){
 
-	myJuke.previousSong();
-});
+// 	myJuke.previousSong();
+// });
