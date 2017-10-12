73d09923bdf05176d13abad11e427a642e521e03
westnorthern <rrichardson610@gmail.com>
2017-10-12 11:08:40 -0600

    added playlist

--- jukebox.js
+++ jukebox.js
@@ -1,8 +1,11 @@
 class Jukebox {
-	constructor(){
-    this.source = "";
-    this.playList = [];
+	constructor(playlist){
+    this.playlist = playlist;
+
+    this.current = this.playlist[0];
+    
     this.audioElement = document.createElement('audio');
+    this.loadSong();
 
 	}
 	playSong(){
@@ -13,22 +16,26 @@ class Jukebox {
 		this.audioElement.pause();
 	}
 	loadSong(){
-		this.audioElement.setAttribute('src', 'songs/Anitek.mp3');
+		this.audioElement.setAttribute('src', this.current);
 
 	}
 
 }
 
+let songArray = ['songs/Anitek.mp3', 'songs/CigBrek.mp3', 
+								'songs/Craze.mp3', 'songs/Math.mp3', 
+								'songs/Rose.mp3', 'songs/Ski.mp3',
+								'songs/Stay.mp3', 'songs/Tonton.mp3', 
+								'songs/Wasaru.mp3'];
 
 
-
-let myJuke = new Jukebox();
+let myJuke = new Jukebox(songArray);
 
 let play = document.getElementById('play');
 let pause = document.getElementById('pause');
-let load = document.getElementById('load');
+let stop = document.getElementById('stop');
 
-load.addEventListener('click', function(){
+stop.addEventListener('click', function(){
 
 	myJuke.loadSong();
 	document.body.style.background = 'red';
